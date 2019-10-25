3f80e81f93081e305d17fc3f2a119d1064975630
Julien Verneaut <jverneaut@gmail.com>
2019-10-25 13:29:55 +0200

    Change contrast formula

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -17,10 +17,11 @@ void main() {
   sampleColor.z = max(0.0, sampleColor.z + u_brightness);
 
   // Contrast
-  sampleColor = u_contrast * (sampleColor - 0.5) + 0.5;
+  sampleColor.x = u_contrast * (sampleColor.x - 0.5) + 0.5;
+  sampleColor.y = u_contrast * (sampleColor.y - 0.5) + 0.5;
+  sampleColor.z = u_contrast * (sampleColor.z - 0.5) + 0.5;
 
   // Saturation
-  float saturation = 1.0;
   float desaturated = (sampleColor.x + sampleColor.y + sampleColor.z) / 3.0;
   sampleColor = (1.0 - u_saturation) * vec4(vec3(desaturated), 1) + u_saturation * sampleColor;
 
